bc5be7dd90a4ee96f056f3e2322629e208ba2508
dhaselwood <dehaselwood@gmail.com>
2024-11-17 12:47:08 -0500

    bypass sql file edit in cangateBMS

--- svn_discoveryf4/PC/sensor/cangateBMS/trunk/cmd_q.c
+++ svn_discoveryf4/PC/sensor/cangateBMS/trunk/cmd_q.c
@@ -128,10 +128,10 @@ int cmd_q_init(char *p)
 			}
 		}
 		printf("Command q: Size of sql file edited = %d\n",wk.size);
-		for (int i = 0; i < wk.size; i++)
-		{
-			printf("%4d 0X%08X %s: %s: %s: %s\n",i,canidsql[i].id,canidsql[i].name,canidsql[i].hex,canidsql[i].type,canidsql[i].msg_fmt);
-		}
+//		for (int i = 0; i < wk.size; i++)
+//		{
+//			printf("%4d 0X%08X %s: %s: %s: %s\n",i,canidsql[i].id,canidsql[i].name,canidsql[i].hex,canidsql[i].type,canidsql[i].msg_fmt);
+//		}
 		cmd_q_initsw = 1;	// Show initialization has been completed		
 	}
 	return 0;

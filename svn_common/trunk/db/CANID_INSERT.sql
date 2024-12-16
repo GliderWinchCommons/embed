2e18514dcb4c44d7ba63b7f976c174ae57058463
dhaselwood <dehaselwood@gmail.com>
2024-12-15 20:09:52 -0500

    update db

--- svn_common/trunk/db/CANID_INSERT.sql
+++ svn_common/trunk/db/CANID_INSERT.sql
@@ -493,7 +493,7 @@ INSERT INTO CANID VALUES ('CANID_CMD_PC_EMC','98000000','UNIT_PC2EMC', 1,1,'U8_U
 -- EMC sends to EMC1, or EMC2
 INSERT INTO CANID VALUES ('CANID_CMD_EMC_EMC','98200000','UNIT_EMC2EMC', 1,1,'U8_U8_U8_X4','EMC sends commands to EMC1 or 2');
 
--- shaft_encoder Mk2 (demo winch) (UNIT: sends readings and CAN loader address; CMD: receives commands)
+-- shaft_encoder Mk2 (demo winch) (UNIT: sends readings and CAN loader address; CMD: receives commands from PC, or HC.)
 INSERT INTO CANID VALUES ('CANID_UNIT_ENCODER1','83200000','UNIT_ENCODER', 1,1,'U8_VAR','DiscoveryF4 encoder demo winch');
 INSERT INTO CANID VALUES ('CANID_UNIT_ENCODER2','83400000','UNIT_ENCODER', 1,1,'U8_VAR','DiscoveryF4 encoder proxy');
 INSERT INTO CANID VALUES ('CANID_CMD_ENCODER1', '83600000','UNIT_ENCODER', 1,1,'U8_VAR','DiscoveryF4 encoder demo winch: command');

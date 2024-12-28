5307a9621f283b1e2ae3f2be487eacb1f5d427de
dhaselwood <dehaselwood@gmail.com>
2024-12-27 19:50:03 -0500

    update CANID_INSERT.sql

--- svn_common/trunk/db/CANID_INSERT.sql
+++ svn_common/trunk/db/CANID_INSERT.sql
@@ -498,8 +498,10 @@ INSERT INTO CANID VALUES ('CANID_UNIT_ENCODER1','83200000','UNIT_ENCODER', 1,1,'
 INSERT INTO CANID VALUES ('CANID_UNIT_ENCODER2','83400000','UNIT_ENCODER', 1,1,'U8_VAR','DiscoveryF4 encoder proxy');
 INSERT INTO CANID VALUES ('CANID_CMD_ENCODER1', '83600000','UNIT_ENCODER', 1,1,'U8_VAR','DiscoveryF4 encoder demo winch: command');
 INSERT INTO CANID VALUES ('CANID_CMD_ENCODER2', '83800000','UNIT_ENCODER', 1,1,'U8_VAR','DiscoveryF4 encoder proxy command');
-INSERT INTO CANID VALUES ('CANID_MSG_ENCODER1', '83A00000','UNIT_ENCODER', 1,1,'FF_FF' ,'DiscoveryF4 encoder demo winch: lineout, speed');
-INSERT INTO CANID VALUES ('CANID_MSG_ENCODER2', '83C00000','UNIT_ENCODER', 1,1,'FF_FF' ,'DiscoveryF4 encoder proxy: lineout, speed');
+INSERT INTO CANID VALUES ('CANID_MSG1_ENCODER1','83A00000','UNIT_ENCODER', 1,1,'FF_FF' ,'DiscoveryF4 encoder demo winch: lineout, speed');
+INSERT INTO CANID VALUES ('CANID_MSG1_ENCODER2','83C00000','UNIT_ENCODER', 1,1,'FF_FF' ,'DiscoveryF4 encoder proxy: lineout, speed');
+INSERT INTO CANID VALUES ('CANID_MSG2_ENCODER1','83E00000','UNIT_ENCODER', 1,1,'FF_FF' ,'DiscoveryF4 encoder demo winch: accel, encoder counter');
+INSERT INTO CANID VALUES ('CANID_MSG2_ENCODER2','84000000','UNIT_ENCODER', 1,1,'FF_FF' ,'DiscoveryF4 encoder proxy: accel, encoder counter');
 
 
 INSERT INTO CANID VALUES ('CANID_UNIT_2', '04000000','UNIT_2', 1,1,'U8','Sensor unit: Drive shaft encoder #1');
